ccacc95fb510ba81df4daa9fa6fbf4d6178dfbb6
keneanung <keneanung@googlemail.com>
2021-10-16 19:37:13 +0200

    Show Double Dip lifeline only, if we are in default game mode

--- src/app/gamemode/gamemode.ts
+++ src/app/gamemode/gamemode.ts
@@ -1,1 +1,15 @@
+import { Injectable } from "@angular/core";
+
 export type ModeType = "default" | "german" | "germanRisk";
+
+@Injectable({
+    providedIn: "root"
+})
+
+export class GameMode {
+    public readonly currentMode: ModeType;
+    constructor(){
+        const urlParams = new URLSearchParams(window.location.search);
+        this.currentMode = <ModeType>urlParams.get("gameMode")
+    }
+}
